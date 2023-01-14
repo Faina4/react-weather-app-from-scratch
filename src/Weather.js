8acd1b20406563e65db06e4f7d757ef67f1b5e9e
FaIna <105994688+Faina4@users.noreply.github.com>
2023-01-14 16:06:37 +0200

    Making Css structure

--- src/Weather.js
+++ src/Weather.js
@@ -8,22 +8,41 @@ export default function Weather(){
                 <li>  Weekday&time Saturday 00:00 </li>
                
              </ul>  
-            
-             <h1>
+             <div className="row current-city-search">
+                <div className="col-4 p-0">
+             <h1 className="my-1 p-0">
                 Default City
              </h1>
-             
+             </div>
+             <div className="col-8">
+                <form className="row">
+               
+                <div className="col-9 pe-1">
+                    <input type="search" placeholder="Search a city..." className="form-control" autoFocus="on">
+                    </input> </div>
+                     <div className="col-3 ps-1">
+                    <input type="submit" value="Search" className="btn btn-personal w-100">
+                    </input>
+                    </div>
+                   
+                </form>
+             </div>
+             </div>
              <ul>
                 <div className="row">
                 <div className="col-6">
-             <li>  
+             <li className="main-temp">  
                 <img src="https://openweathermap.org/img/wn/04d@2x.png" alt="cloudy">
-                </img> Temperature: 5C
+                </img> 
+                <span className="temperature">Temperature: 8 </span>
+                 <span className="units"> °C |°F </span> 
+              
              </li>
-                <li>  Description: </li>
+               
                 
                 </div>
-                <div className="col-6">
+                <div className="col-6 main-temp-description align-middle">
+                <li>   Description: cloudy   </li>
                 <li>  Humidity:  </li>
                 <li>  Wind speed:  </li>
                 </div>
