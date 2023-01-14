96357a6be03b923c47a5782cead13cf4a35ed1cf
FaIna <105994688+Faina4@users.noreply.github.com>
2023-01-14 11:18:41 +0200

    Added repo URL

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
      
         <h1> Weather Forecast</h1>
        <footer>
-        This app was coded by Anna Filyakina and is open-sourced on GitHub
+        This app was coded by Anna Filyakina and is  {""}<a href="https://github.com/Faina4/react-weather-app-from-scratch-hw5">
+open-sourced
+</a>
+{""} on GitHub
        </footer>
     </div>
   );
