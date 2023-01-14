9b130f1be69ebc56a90a09ae957c1da566508b36
FaIna <105994688+Faina4@users.noreply.github.com>
2023-01-14 11:09:28 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
      
         <h1> Weather Forecast</h1>
-       
+       <footer>
+        This app was coded by Anna Filyakina and is open-sourced on GitHub
+       </footer>
     </div>
   );
 }
