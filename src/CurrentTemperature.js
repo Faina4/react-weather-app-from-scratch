c1028b908d92f34627f899d9f480bc44d379fbb3
FaIna <105994688+Faina4@users.noreply.github.com>
2023-01-17 00:08:13 +0200

    Added convertion of temperature from celsius to farehrenheit

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -3,23 +3,40 @@ import "./CurrentTemperature.css";
 //import { useState } from "react";
 
 export default function CurrentTemperature(props){
-    const[temperature, setTemperature]=useState(props.celsius)
-   
-    function showCelsius(event){
-        event.preventDefault();
-        setTemperature(props.temperature)
-    } 
+    const[unit, setUnit]=useState("celsius");
+    
+
+ function showCelsius(event){
+    event.preventDefault();
+   setUnit("celsius")   
+     } 
+
+
     function showFarehrenheit(event){
         event.preventDefault();
-        setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+        setUnit("farehrenheit")
+        //((props.celsius * 9) / 5 + 32);
+        
     }
-    return (      
+if (unit==="celsius"){
+      return (      
         
     <span className="CurrentTemperature "> 
-             <span className="temperature "> {temperature} </span>
+             <span className="temperature "> {Math.round(props.celsius)} </span>
             <span className="units"> 
-            <a href="/" onClick={showCelsius}>°C</a>  | <a href="/" onClick={showFarehrenheit}>°F </a></span>   
+            <a href="/" >°C</a>  | <a href="/" onClick={showFarehrenheit}>°F </a></span>   
     </span>
          
     );
+}else{
+    let farehrenheit=(props.celsius * 9) / 5 + 32;
+    return ( <span className="CurrentTemperature "> 
+    <span className="temperature "> {Math.round(farehrenheit)} </span>
+   <span className="units"> 
+   <a href="/" onClick={showCelsius}>°C</a>  | <a href="/" >°F </a></span>   
+</span>)
+}
+
+   
+  
 }
