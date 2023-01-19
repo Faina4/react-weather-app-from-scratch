c76ba33084b016b7256e93c9dffffb52062f860d
FaIna <4511228@gmail.com>
2023-01-20 01:09:07 +0200

    Week days added:
    -added weekdays - 3 letters
    -function day()

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,6 +2,17 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props){
+function day(){
+    let date = new Date(props.dailyData.dt*1000);
+    let day = date.getDay();
+
+    let days=[
+        "Sun",  "Mon","Tue", "Wed", "Thu", "Fri", "Sat", 
+    ]
+    return days[day];
+}
+
+
 function maxTemperature(){ //Math.round max temp data and add as a function (?)
     let temperature = Math.round(props.dailyData.temp.max)
     return `${temperature}°`
@@ -15,7 +26,7 @@ function minTemperature(){
 <div>
 
 
-        <div className="DailyForecast-day">  {props.dailyData.dt} </div>
+        <div className="DailyForecast-day">  {day()} </div>
         <WeatherIcon  code={props.dailyData.weather[0].icon} size={32} />
         <div>
         <span className="DailyForecastTemp-max"> {maxTemperature()}</span> 
