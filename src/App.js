0e2321b344098f7d9712df4e7c83aea939d8c042
FaIna <4511228@gmail.com>
2023-01-20 03:12:29 +0200

    App.js: added Netlify site link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
      <Weather defaultCity="Kyiv" />
     
            <footer>
- This app was coded by FaIna and is on{""}<a href="https://github.com/Faina4/react-weather-app-from-scratch-hw5" rel="noreferrer" target="_blank"> GitHub </a>
+ This app was coded by FaIna and is on{""}<a href="https://github.com/Faina4/react-weather-app-from-scratch-hw5" 
+ rel="noreferrer" target="_blank"> GitHub </a>{""} and {""}
+ hosted on <a href="https://relaxed-pika-1f6901.netlify.app" target="_blank" rel="noreffer" > Netlify</a>
+
      
            </footer>
        </div>
