8b9981987bd5ec9f7241d703965110b1684485ed
FaIna <4511228@gmail.com>
2023-01-20 03:50:45 +0200

    netlify link revised
     rel="noopener noreferrer"

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
            <footer>
  This app was coded by FaIna and is on{""}<a href="https://github.com/Faina4/react-weather-app-from-scratch-hw5" 
  rel="noreferrer" target="_blank"> GitHub </a>{""} and {""}
- hosted on <a href="https://relaxed-pika-1f6901.netlify.app" target="_blank" rel="noreffer" > Netlify</a>
+ hosted on <a href="https://relaxed-pika-1f6901.netlify.app" target="_blank"  rel="noopener noreferrer" > Netlify</a>
 
      
            </footer>
